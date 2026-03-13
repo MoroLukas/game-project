8b2894752efb11a061d29aca7509229208f7eaa6
vietforest <nguyenhuynhanh1954@gmail.com>
2026-03-13 08:42:53 +0100

    super update

--- 5_Applicativo/Slime and Guns/Assets/Scripts/Menu/OptionMenu.cs
+++ 5_Applicativo/Slime and Guns/Assets/Scripts/Menu/OptionMenu.cs
@@ -1,12 +1,26 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Audio;
+using static UnityEngine.Rendering.DebugUI;
 public class OptionMenu : MonoBehaviour
 {
-
-    public AudioMixer audioMixer;
+    [SerializeField]
+    private AudioMixer Mixer;
+  
+    [SerializeField]
+    private AudioSource AudioSource;
+    private void Start()
+    {
+        Mixer.SetFloat("Volume", Mathf.Log10(PlayerPrefs.GetFloat("Volume", 1) * 20));
+    }
 
     public void SetVolume(float volume)
     {
-        audioMixer.SetFloat("Volume", volume);
+        Mixer.SetFloat("Volume", Mathf.Log10(volume)*20);
+        float a = Mathf.Log10(volume) * 20;
+
+        PlayerPrefs.SetFloat("Volume", volume);
+        PlayerPrefs.Save();
+
     }
 }
