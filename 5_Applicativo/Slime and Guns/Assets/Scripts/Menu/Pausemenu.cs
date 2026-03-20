ff0ea12c63c470ab352718cafa99eabe58927e49
Lukas <zio387@student.edu.ti.ch>
2026-03-20 10:58:15 +0100

    Update PauseMenu

---
+++ 5_Applicativo/Slime and Guns/Assets/Scripts/Menu/Pausemenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Pausemenu : MonoBehaviour
+{
+
+    public GameObject container;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
