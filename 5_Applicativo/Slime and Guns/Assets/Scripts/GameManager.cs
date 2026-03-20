65d0ac928c28f970ff77757a86ee054cc26dc3ba
Lukas <zio387@student.edu.ti.ch>
2026-03-20 13:16:34 +0100

    Update GameManager

--- 5_Applicativo/Slime and Guns/Assets/Scripts/GameManager.cs
+++ 5_Applicativo/Slime and Guns/Assets/Scripts/GameManager.cs
@@ -1,10 +1,40 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField] private PlayerHealth playerHealth;
+    [SerializeField] private GameObject gameOverScreen;
+
     void Awake()
     {
         QualitySettings.vSyncCount = 0;      // Disattiva VSync
-        Application.targetFrameRate = 60;    // Blocca a 60 FPS
+        Application.targetFrameRate = 60;
+        if (Instance != null) { Destroy(gameObject); return; }
+        Instance = this;
+    }
+
+    void Start()
+    {
+        playerHealth.OnDeath += HandleGameOver;
+    }
+
+    void OnDestroy()
+    {
+        playerHealth.OnDeath -= HandleGameOver;
+    }
+
+    private void HandleGameOver()
+    {
+        gameOverScreen.SetActive(true);
+        Time.timeScale = 0f; // pausa il gioco
+    }
+
+    public void RestartRun()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
